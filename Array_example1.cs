1eb12e8db904474715d4de0f8fbcca3bd287cc98
sinbak <sinbato03@naver.com>
2022-02-19 20:45:44 +0900

    C# array1

---
+++ Array_example1.cs
@@ -0,0 +1,24 @@
+// namespace Array_example1
+// {
+//     class Array_example1
+//     {
+//         static void Main(string[] args)
+//         {
+//             int[] nArray = {1,2,3,4};
+//             for(int i = 0; i < 4; i++)
+//                 Console.Write("{0} ", nArray[i]);
+//             Console.Write('\n');
+
+//             for(int i = 0; i < nArray.Length; i++)
+//                 Console.Write("{0} ", nArray[i]);
+//             Console.Write('\n');
+
+//             foreach(int m in nArray)
+//                 Console.Write("{0} ", m);
+//             Console.Write('\n');
+//             string[] Days = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+//             foreach(string str in Days)
+//                 Console.Write(str + " ");
+//         }
+//     }
+// }
--- Grammer4_while.cs
+++ Grammer4_while.cs
@@ -1,22 +1,22 @@
-using System;
+// using System;
 
-namespace Grammer4_while
-{
-    class Gram
-    {
-        static void Main(string[] args)
-        {
-            int i = 0;
-            int sum = 0;
+// namespace Grammer4_while
+// {
+//     class Gram
+//     {
+//         static void Main(string[] args)
+//         {
+//             int i = 0;
+//             int sum = 0;
 
-            while(i<=100)
-            {
-                i++;
-                sum += i;
-                Console.WriteLine(sum);
-            }
-            Console.WriteLine("Press any key...");
-            Console.Read();
-        }
-    }
-}
+//             while(i<=100)
+//             {
+//                 i++;
+//                 sum += i;
+//                 Console.WriteLine(sum);
+//             }
+//             Console.WriteLine("Press any key...");
+//             Console.Read();
+//         }
+//     }
+// }
