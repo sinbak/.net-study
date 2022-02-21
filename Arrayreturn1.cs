98a7bd3d0b61a576a479579d36c79e33aed2c6f0
sinbak <sinbato03@naver.com>
2022-02-21 19:22:13 +0900

    C# reuturn Array

---
+++ Arrayreturn1.cs
@@ -0,0 +1,42 @@
+namespace Arrayreturn1
+{
+    class Arrayreturn1
+    {
+        static int[] CreateArray1(int nSize)
+        {
+            int[] Array1 = new int[nSize];
+            for (int i = 0; i < Array1.Length; i++)
+                Array1[i] = i;
+            return Array1;
+        }
+
+        static int[,] CreateArray2(int nRow, int nCol)
+        {
+            int index = 0;
+            int[,] Array2 = new int[nRow, nCol];
+            for (int i = 0; i < nRow; i++)
+                for (int j = 0; j < nCol; j++)
+                    Array2[i,j] = index++;
+
+            return Array2;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] nArray1;
+            int[,] nArray2;
+
+            nArray1 = CreateArray1(5);
+            nArray2 = CreateArray2(2,3);
+
+            for (int i = 0; i < nArray1.Length; i++)
+                Console.Write(nArray1[i]);
+            Console.WriteLine();
+            for (int i = 0; i < 2; i++)
+                for (int j = 0; j < 3; j++)
+                    Console.Write(nArray2[i, j]);
+            Console.WriteLine();
+        }
+    }
+}
